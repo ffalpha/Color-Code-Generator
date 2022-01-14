0cb49826826ad24559d9f9222e2c35da6a936e07
ffalpha <kalanam217@gmail.com>
2022-01-14 19:46:40 +0530

    Add css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("");
+  const [error,setError]=useState(false);
   return (
-    <div className="App">
-vx
+    <div>
+      <section className="container">
+        <h3>Hex Color Genrator </h3>
+        <form>
+          <input
+            type="text"
+            placeholder="type color"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            className={`${error ? "error":null}`}
+          />
+          <button  className="btn" type="submit">Get Colors</button>
+        </form>
+      </section>
     </div>
   );
 }

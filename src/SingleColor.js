47d55c7a5577662100c759e158bae73c715fd9d7
ffalpha <kalanam217@gmail.com>
2022-01-15 11:05:33 +0530

    Add copy function

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import rgbToHex from './utils'
 
 function SingleColor({rgb,weight,index,hexColor}) {
     const [alert,setAlert] = useState(false)
     const bcg =rgb.join(",")
     const hex =rgbToHex(...rgb) ///copy each and eveyrone
+    const hexValue=`#${hexColor}`
+
+    useEffect(()=>{
+        const timeout= setTimeout(()=>{
+            setAlert(false)
+        },3000)
+        return () =>clearTimeout(timeout)
+    },[alert])
     return (
-      <article className={`color ${index > 10 && 'color-light'}`} style={{background:`rgb(${bcg})`}}>
+      <article className={`color ${index > 10 && 'color-light'}`} style={{background:`rgb(${bcg})`}} onClick={()=>{setAlert(true);navigator.clipboard.writeText(hexValue)}}>
             <p className="percentage-value">{weight}</p>
-            <p className="color-value">{hexColor}</p>
+            <p className="color-value">{hexValue}</p>
+            {alert && <p className='alert'>Copied to clipborad</p>}
       </article>
     )
 }
